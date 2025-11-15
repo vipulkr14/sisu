c4d74a35015d221020811579b65799e0a01da0d4
Vipul Kumar <vipulvk.kumar@gmail.com>
2025-11-15 21:48:35 +0200

    modified APIs

--- process.py
+++ process.py
@@ -36,9 +36,9 @@ def call_webhook_security_assessment(product=None, company=None, url_param=None)
     response = send_post_request(url, final_payload)
 
     #TODO: Process this response to get the calculable value for scoring
-
+    return response
     # Return only the JSON payload, or None if request failed
-    return response.get("response_json") if response.get("success") else None
+    # return response.get("response_json") if response.get("success") else None
 
 def call_webhook_license_scan(product=None, company=None, url_param=None):
     """
@@ -59,15 +59,16 @@ def call_webhook_license_scan(product=None, company=None, url_param=None):
     # Return only the JSON payload, or None if request failed
     return response.get("response_json") if response.get("success") else None
 
-def call_webhook_virustotal(product=None, company=None, url_param=None):
+def call_webhook_virustotal(product=None, company=None, sha1=None):
     """
     Sends a POST request with JSON content containing
     three parameters. Supports missing/None values.
     """
-    url = "https://plantbase.app.n8n.cloud/webhook/virustotal"
+    url = "https://plantbase.app.n8n.cloud/webhook/virustotal-check"
     payload = {
         "product_name": product,
-        "website": url_param,
+        "company_name": company,
+        "hash": sha1
     }
     final_payload = {"product": payload}
     response = send_post_request(url, final_payload)
@@ -82,7 +83,7 @@ def call_webhook_certs_scan(product=None, company=None, url_param=None):
     Sends a POST request with JSON content containing
     three parameters. Supports missing/None values.
     """
-    url = "https://plantbase.app.n8n.cloud/webhook/certs-scan"
+    url = "https://plantbase.app.n8n.cloud/webhook/8c532972-bfd6-40dc-9ff2-9cb0dab6d135"
     payload = {
         "product_name": product,
         "website": url_param,
@@ -124,6 +125,7 @@ def send_post_request(url, payload):
 
 def get_cve_data_summary(product=None, company=None, sha1=None):
     result_json = cve.get_cve_records_by_keyword(product_name=product,use_exact_match=False)
+    return result_json
 
 # --------------------------
 # Placeholder Graph Generator
@@ -158,18 +160,18 @@ def prepare_final_result(product=None, company=None, url_param=None):
 
 
 # for testing only
-def main():
-    # product='1Password'
-    # company='1Password'
-    # sha1='e5ee385388b5fa57cc8374102d779d3c9849a57f'
-    # response = call_webhook_assess_product(product=product,company=company,sha1=sha1)
-    # print(response)
-    product="PeaZip"
-    company="Giorgio Tani"
-    url="https://github.com/peazip/PeaZip"
-    # response=call_webhook_license_scan(product=product,company=company,url_param=url)
-    response=call_webhook_security_assessment(product=product,company=company,url_param=url)
-    print(response)
+# def main():
+#     # product='1Password'
+#     # company='1Password'
+#     # sha1='e5ee385388b5fa57cc8374102d779d3c9849a57f'
+#     # response = call_webhook_assess_product(product=product,company=company,sha1=sha1)
+#     # print(response)
+#     product="Zoom"
+#     company="Zoom Video Communications, Inc."
+#     url="fd797e4071afe131104c1d29cd0cb606da62f3d5"
+#     # response=call_webhook_license_scan(product=product,company=company,url_param=url)
+#     response=get_cve_data_summary(product=product,company=company,sha1=url)
+#     print(response)
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
